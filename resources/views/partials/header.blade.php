a198947290da92eb72558491bc71929ae21fde46
attraverso <attraverso.dev@gmail.com>
2020-06-30 16:37:21 +0200

    header draft complete

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+  <div class="header-left">
+    <img src="http://boolean.careers/images/common/logo.png" alt="">
+  </div>
+  <nav class="header-right">
+    <ul>
+      <li>Home</li>
+      <li>Corso</li>
+      <li>Dopo il corso</li>
+      <li>Lezione gratuita</li>
+      <li>Assumi i nostri studenti</li>
+      <li class="btn btn-primary">Candidati ora</li>
+    </ul>
+  </nav>
+</header>
